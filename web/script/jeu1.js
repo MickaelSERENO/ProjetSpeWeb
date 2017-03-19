91051c20672968bff0e31c267b1ad9aa381716a7
Gaulois94 <serenomickael@gmail.com>
2017-03-19 23:20:24 +0000

    Continue to develop game 1

--- web/script/jeu1.js
+++ web/script/jeu1.js
@@ -9,25 +9,39 @@ function promptSentences(idPackSentence, idSentence, callback, data=null)
 			callback(data, receive);
 		}
 	}
-	httpCtx.overrideMimeType('application/json');
-	httpCtx.open("POST", "handlingGame1.php", true);
+	httpCtx.open("POST", "ClientQuery/handlingGame1.php", true);
 	httpCtx.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	httpCtx.send("idPrompt=1&idPack="+idPackSentence+"$idSent="+idSentence);
+	httpCtx.send("idPrompt=1&idPack="+idPackSentence+"&idSent="+idSentence);
 }
 
-function getSentencesFromServer(sent, jsonData)
+function getSentencesFromServer(ctx, jsonData)
 {
-	print(jsonData);
+	drawSentence(ctx, jsonData.sent1, 0, 100);
+	drawSentence(ctx, jsonData.sent2, 0, 300);
 }
 
 function drawSentence(ctx, sentence, px, py)
 {
+	ctx.font="25px Arial";
+	ctx.strokeStyle = "Green";
+	var spaceMeasure = ctx.measureText("       ").width;
+	px += spaceMeasure/3.0;
+	var fontHeight = 35;
+	var currentX = 0;
+
+	for(var i=0; i < sentence.wordArray.length; i++)
+	{
+		ctx.fillText(sentence.wordArray[i].groupText, px+currentX, py);
+		var measure = ctx.measureText(sentence.wordArray[i].groupText).width;
+		ctx.strokeRect(px+currentX-spaceMeasure/3.0, py-fontHeight+5, measure+spaceMeasure - spaceMeasure/3.0, fontHeight);
+		currentX += measure+spaceMeasure;
+	}
 }
 
-//$locationProvider.html5Mode(true);
-
-//var canv = document.getElementById('canvasJeu1');
-//var ctx  = canv.getContext('2d');
-//var packSentenceID = location.search().packSentence();
-
-promptSentences(2, 1, getSentencesFromServer);
+//Le main
+window.onload = function()
+{
+	var canv = document.getElementById('canvasJeu1');
+	var ctx  = canv.getContext('2d');
+	promptSentences(2, 1, getSentencesFromServer, ctx);
+}
