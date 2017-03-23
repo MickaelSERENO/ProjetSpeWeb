da9ec8bd8891febad23b2d09d4e27f9c35d84db4
Gaulois94 <serenomickael@gmail.com>
2017-03-23 15:46:00 +0000

    Statistic php sources page

---
+++ web/statistics.php
@@ -0,0 +1,45 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<script src="bower_components/angular/angular.min.js"></script>
+		<script src="bower_components/xmlhttprequest/XMLHttpRequest.js"></script>
+		<script src="script/stats.js"></script>
+	</head>
+
+	<body ng-app="statsApp">
+<?php
+		class LangContent
+		{
+			public $listStats;
+		}
+
+		//Load symfony
+		require_once __DIR__.'/../vendor/autoload.php';
+
+		//Get serializer XML
+		use Symfony\Component\Serializer\Serializer;
+		use Symfony\Component\Serializer\Encoder\XmlEncoder;
+		use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+		$encoders = array(new XmlEncoder());
+		$normalizers = array(new ObjectNormalizer());
+		$serializer = new Serializer($normalizers, $encoders);
+
+		$listStatsText = file_get_contents("../res/lang/fr/statistic.xml");
+		$langData      = $serializer->deserialize($listStatsText, LangContent::class, 'xml');
+		$listStats     = $langData->listStats;
+
+		echo <<< EOT
+			<div ng-controller="listStatsCtrl">
+				<statsAccordion>
+					<stats-expander title='$listStats[listStutends]'></stats-expander>
+					<stats-expander title='$listStats[classement]'></stats-expander>
+					<stats-expander title='$listStats[history]'></stats-expander>
+				</statsAccordion>
+			</div>
+		EOT;
+?>
+	</body>
+</html>
