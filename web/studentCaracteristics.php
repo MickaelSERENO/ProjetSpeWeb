1f30b97cce9fdba0ccc776e78ce2469a521048ab
Gaulois94 <serenomickael@gmail.com>
2017-04-02 02:35:35 +0200

    Include header and footer

--- web/studentCaracteristics.php
+++ web/studentCaracteristics.php
@@ -9,9 +9,16 @@
 		<script src="bower_components/xmlhttprequest/XMLHttpRequest.js"></script>
 	</head>
 
+	<header class="headerAcc">
+		<?php include('HeaderFooter/Header.inc.php'); ?>
+	</header>
+
 	<body ng-app="studChaApp">
 <?php
-
 ?>
 	</body>
+
+	<footer>
+		<?php include('HeaderFooter/Footer.inc.php'); ?>
+	</footer>
 </html>
