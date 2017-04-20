982e93bfeed7eb7adba0873cc64597e00eed7c9c
Tlospock <meunierpaul@free.fr>
2017-04-20 23:28:06 +0200

    useless commit

--- web/Accueil/Accueil.php
+++ web/Accueil/Accueil.php
@@ -1,11 +1,34 @@
 <?php session_start();?>
+<?php
+	class LangContent
+	{
+		public $txt_acc;
+	}
+	
+	//Load symfony
+	require_once __DIR__.'/../../vendor/autoload.php';
+	require_once __DIR__.'/../ClientQuery/PSQLDatabase.php';
+
+	//Get serializer XML
+	use Symfony\Component\Serializer\Serializer;
+	use Symfony\Component\Serializer\Encoder\XmlEncoder;
+	use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+	
+	$encoders = array(new XmlEncoder());
+	$normalizers = array(new ObjectNormalizer());
+	$serializer = new Serializer($normalizers, $encoders);
+	
+	$listStatsText = file_get_contents("../res/lang/fr/Accueil_fr.xml");
+	$langData      = $serializer->deserialize($listStatsText, LangContent::class, 'xml');
+	$txt_acc = $langData->txt_acc;
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" charset="UTF-8">
 	<head>
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="/CSS/Accueil.css" />
 		<link rel="SHORTCUT ICON" href="/res/Img/IcoBal.ico">
-		<title>Albatros Sensei: Accueil</title>
+		<title><?php echo ("$txt_acc[title]"); ?></title>
 	</head>
 	
 	<header class="headerAcc">
@@ -15,7 +38,7 @@
 	<body>
 	<br/>
 		<div class="backgroundBody">
-			<h1 class="acc"> Bienvenue sur ce site web funtastic! </h1>
+			<h1 class="acc"> <?php echo ("$txt_acc[phrase_acc]"); ?> </h1>
 			<section class="presentationAcc">
 				<h3>
 					Présentation du site!
